7e17f18cb9fe023a95c9121143b08f219c3058d7
Jorge Valdes <jav165@miami.edu>
2020-04-06 16:38:43 -0400

    updated recipe hw

--- Homework/recipe/jquery/recipe.js
+++ Homework/recipe/jquery/recipe.js
@@ -1,19 +1,20 @@
 $(document).ready( function(){
 
   $("#_hide").click(function(){
-    $(".ingredients div:first-child ").hide();
+    $(".ingredients").hide();
   });
 
 $("#_html").click(function() {
-  $(".introp div:first-child").html("Enjoy your Meal!");
+  $(".introp").html("Enjoy your Meal!");
 });
 
 
-  $("#_Dark").click(function(){
+$("#_show").click(function(){
+  $(".ingredients").show();
+});
 
-        $("#Body").toggleClass("dark", true);
-        $("#Body").toggleClass("light", false);
 });
 $("#_css").click(function(){
   $(".container2").css("background-color", "green");
-})});
+});
+});
