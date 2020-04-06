12fb63e0a04825cac5b84e3e199a134c96114443
Jorge Valdes <jav165@miami.edu>
2020-04-06 16:46:01 -0400

    final recipe website

--- Homework/recipe/jquery/recipe.js
+++ Homework/recipe/jquery/recipe.js
@@ -7,13 +7,11 @@ $(document).ready( function(){
 $("#_html").click(function() {
   $(".introp").html("Enjoy your Meal!");
 });
-
-
 $("#_show").click(function(){
   $(".ingredients").show();
 });
 
-});
+
 $("#_css").click(function(){
   $(".container2").css("background-color", "green");
 });
