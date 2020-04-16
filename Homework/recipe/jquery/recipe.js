ba83a8ea2aa51ddf25f480e0c7c08d4bd20b5e18
Jorge Valdes <jav165@miami.edu>
2020-04-16 13:44:05 -0400

    trying to fix plugin

--- Homework/recipe/jquery/recipe.js
+++ Homework/recipe/jquery/recipe.js
@@ -11,14 +11,12 @@ $("#_show").click(function(){
   $(".ingredients").show();
 });
 $(".title").lettering();
-   });
-   jQuery("#nanoGallery1").nanoGallery({
- 		kind:'flickr',
- 		userID:'34858669@N00'
- 	});
- });
- $('#_pagepiling').pagepiling();
 });
+   $('#pagepiling').pagepiling();
+ });
+$   ("nanoGallery2")
+});
+
 $("#_css").click(function(){
   $(".container2").css("background-color", "green");
 });
