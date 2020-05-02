521d382b9e660bfc69616a16ca6690f6ef1d148a
Jorge Valdes <jav165@miami.edu>
2020-05-02 14:54:48 -0400

    final 5

---
+++ Homework/Final Project/js/jsfile.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  jQuery("#nanogallery2").nanogallery2( {
+                         // ### gallery settings ###
+                         thumbnailHeight:  150,
+                         thumbnailWidth:   150,
+                         itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+
+                       });
