4c3960de62126b8e35e5b255a01bbeb6554dce69
Jorge Valdes <jav165@miami.edu>
2020-05-02 15:17:10 -0400

    trying jquery functions

--- Homework/Final Project/js/jsfile.js
+++ Homework/Final Project/js/jsfile.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  jQuery("#nanogallery2").nanogallery2( {
-                         // ### gallery settings ###
-                         thumbnailHeight:  150,
-                         thumbnailWidth:   150,
-                         itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-
-                       });
+  $("button").click(function(){
+  $("li").hide();
+});
+});
