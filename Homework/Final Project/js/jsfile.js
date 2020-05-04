6237c3b4d1ad293771550d968eb83fde25843170
Jorge Valdes <jav165@miami.edu>
2020-05-04 11:59:29 -0400

    trying to fix jquery

--- Homework/Final Project/js/jsfile.js
+++ Homework/Final Project/js/jsfile.js
@@ -1,4 +1,17 @@
-$(document).ready( function() {
-  $("button").click(function(){
-  $(".basicGrid").hide();
-});
+$(document).ready(function(){
+  $('.photoset-grid-custom').photosetGrid({
+    // Set the gutter between columns and rows
+    gutter: '5px',
+    // Manually set the grid layout
+    layout: '21',
+    // Wrap the images in links
+    highresLinks: true,
+    // Asign a common rel attribute
+    rel: 'print-gallery',
+
+    onInit: function(){},
+    onComplete: function(){
+      // Show the grid after it renders
+      $('.photoset-grid-custom').attr('style', '');
+    }
+  });
